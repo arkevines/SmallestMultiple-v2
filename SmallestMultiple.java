99f53654cbf35464cdc95fb999e1184bfb8b4a2b
bs93 <bs93@interia.pl>
2015-05-28 18:46:58 +0200

    Rozpoczęcie projektu
    Dodanie testu dla metody wyszukujacej najmniejszą wspólną wielokrotność

---
+++ SmallestMultiple.java
@@ -0,0 +1,16 @@
+package smallestmultiple;
+
+/**
+ *
+ * @author Szmolke
+ */
+public class SmallestMultiple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
